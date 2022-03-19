1dc70508aeca4b2ec72f6e25e9b16c21aa808a02
andrew-aiken <legolemur@gmail.com>
2022-03-19 18:53:56 -0400

    rename

--- DeathswapPlus/src/functions/DeathSwap/tick.ts
+++ DeathswapPlus/src/functions/DeathSwap/tick.ts
@@ -1,6 +1,6 @@
 import { MCFunction, execute, say } from 'sandstone'
 
-MCFunction('reload', () => {
+MCFunction('tick', () => {
   execute.if.entity('@e[type=armor_stand,name=Values,scores={GameStart=0}]').run.functionCmd('deathswap-plus:deathswaplus/lobby')
 
   execute.if.entity('@e[type=armor_stand,name=Values,scores={GameStart=1}]').run.functionCmd('deathswap-plus:deathswaplus/in_game')
