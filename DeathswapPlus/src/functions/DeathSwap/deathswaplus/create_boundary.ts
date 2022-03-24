411ccb223acce6f2742c14cc7357d05be0683e9d
andrew-aiken <legolemur@gmail.com>
2022-03-24 09:18:53 -0400

    something to test

--- DeathswapPlus/src/functions/DeathSwap/deathswaplus/create_boundary.ts
+++ DeathswapPlus/src/functions/DeathSwap/deathswaplus/create_boundary.ts
@@ -5,15 +5,11 @@ MCFunction('deathswaplus/create_boundary', () => {
   kill('@e[type=minecraft:armor_stand,name=Border]')
   execute.at('@r').run.summon('minecraft:armor_stand', ['~', '~', '~'], {CustomName:"{\"text\":\"Border\"}"})
   
-  execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).run.say('Matches 1')
+  //execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).run.say('Matches 1')
+  execute.if.entity('@e[type=minecraft:armor_stand,name=Values,scores={S_ValidZone=1..}]').run.say('Matches 1')
 
-  execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).run.functionCmd('deathswapplus:deathswaplus/create_boundary/while')
-
-  /*
-  _.while(_.data.entity('@s', 'SelectedItem{id:"minecraft:stick"}'), () => {
-    say('Hey! Nice stick you got there.')
-  })
-*/
+  //execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).run.functionCmd('deathswapplus:deathswaplus/create_boundary/while')
+  execute.if.entity('@e[type=minecraft:armor_stand,name=Values,scores={S_ValidZone=1..}]').run.functionCmd('deathswapplus:deathswaplus/create_boundary/while')
 
   //scoreboard.players.set('@e[type=minecraft:armor_stand,name=Values]', 'S_ValidZone', 1)
 })
@@ -21,9 +17,21 @@ MCFunction('deathswaplus/create_boundary', () => {
 MCFunction('deathswaplus/create_boundary/while', async () => {
   say('---')
   await sleep('1s')
-  execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).at('@e[type=minecraft:armor_stand,name=Border,limit=1]').run.tp('@e[type=minecraft:armor_stand,name=Border,limit=1]', ['~1', '~', '~'])
-  execute.at('@e[type=minecraft:armor_stand,name=Border,limit=1]').if.block(['~', '~-1', '~'], 'minecraft:sandstone').run.scoreboard.players.set('@e[type=minecraft:armor_stand,name=Values]', 'S_ValidZone', 0)
-  execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).run.functionCmd('deathswapplus:deathswaplus/create_boundary/while')
+  //execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).at('@e[type=minecraft:armor_stand,name=Border,limit=1]').run.tp('@e[type=minecraft:armor_stand,name=Border,limit=1]', ['~1', '~', '~'])
+  execute.if.entity('@e[type=minecraft:armor_stand,name=Values,scores={S_ValidZone=1..}]').at('@e[type=minecraft:armor_stand,name=Border,limit=1]').run.tp('@e[type=minecraft:armor_stand,name=Border,limit=1]', ['~1', '~', '~'])
+  //execute.at('@e[type=minecraft:armor_stand,name=Border,limit=1]').if.block(['~', '~-1', '~'], 'minecraft:sandstone').run.scoreboard.players.set('@e[type=minecraft:armor_stand,name=Values]', 'S_ValidZone', 0)
+  
+  //execute.if.score('@e[type=minecraft:armor_stand,name=Values,limit=1]', 'S_ValidZone', 'matches', 1).run.functionCmd('deathswapplus:deathswaplus/create_boundary/while')
+  execute.if.entity('@e[type=minecraft:armor_stand,name=Values,scores={S_ValidZone=1..}]').if.block(['~', '~-1', '~'], 'minecraft:water').run.functionCmd('deathswapplus:deathswaplus/create_boundary/while')
+  execute.if.entity('@e[type=minecraft:armor_stand,name=Values,scores={S_ValidZone=0}]').run.functionCmd('deathswapplus:deathswaplus/create_boundary/border')
+})
+
+MCFunction('deathswaplus/create_boundary/border', async () => {
+  execute.at('@e[type=minecraft:armor_stand,name=Border,limit=1]').run.setworldspawn(['~', '~', '~'])
+  execute.at('@e[type=minecraft:armor_stand,name=Border,limit=1]').run.worldborder.center(['~', '~', '~'])
+  kill('@a')
+  execute.at('@e[type=minecraft:armor_stand,name=Border,limit=1]').run.worldborder.damageBuffer(20)
+  execute.at('@e[type=minecraft:armor_stand,name=Border,limit=1]').run.worldborder.set(10, 0)
 })
 
 /*
--- DeathswapPlus/src/functions/DeathSwap/deathswaplus/setup.ts
+++ DeathswapPlus/src/functions/DeathSwap/deathswaplus/setup.ts
@@ -21,11 +21,11 @@ MCFunction('deathswaplus/setup', () => {
   // Start the Game //
   execute.if.entity('@e[name=Values,scores={Players=2..}]').run.tellraw('@a', [{"text":"[!]","color":"yellow"},{"text":" Game has started!","color":"gray"}])
   execute.if.entity('@e[name=Values,scores={Players=2..}]').run.scoreboard.players.set('@e[type=armor_stand,name=Values]', 'GameStatus', 1)
+  execute.if.entity('@e[name=Values,scores={Players=2..,S_Border=0}]').run.functionCmd('deathswapplus:deathswaplus/create_boundary')
   execute.if.entity('@e[name=Values,scores={Players=2..}]').run.gamerule('doDaylightCycle', true)
   execute.if.entity('@e[name=Values,scores={Players=2..}]').run.gamemode('survival', '@a[gamemode=adventure]')
   execute.if.entity('@e[name=Values,scores={Players=2..}]').run.advancement.revoke('@a').everything()
   execute.if.entity('@e[name=Values,scores={Players=2..}]').run.scoreboard.players.set('@a', 'Death', 0)
-  execute.if.entity('@e[name=Values,scores={S_Border=0,Players=2..}]').run.functionCmd('deathswapplus:deathswaplus/create_boundary')
   execute.if.entity('@e[name=Values,scores={Players=2..}]').at('@a').run.playsound('minecraft:entity.arrow.hit_player', 'master', '@p', ['~ ~ ~'], 100, 1)
 
   // No enough Players //
@@ -35,11 +35,4 @@ MCFunction('deathswaplus/setup', () => {
   execute.if.entity('@e[name=Values,scores={Players=1}]').run.tellraw('@a', [{"text":"[!] ","color":"yellow"},{"text":"Click here to start the game!","color":"gray","clickEvent":{"action":"run_command","value":"/trigger Select set 1"}}])
   execute.if.entity('@e[name=Values,scores={Players=1}]').run.tellraw('@a', [{"text":" "}])
   execute.if.entity('@e[name=Values,scores={Players=1}]').run.tellraw('@a', [{"text":"[!] ","color":"yellow"},{"text":"Click here to change the settings!","color":"gray","clickEvent":{"action":"run_command","value":"/trigger Select set 2"}}])
-
-  /*
-    For each team
-    take one player (@r) from the 'teamless' and assign to team X
-
-    -- Based on that people can be in multiple teams
-  */
 })
